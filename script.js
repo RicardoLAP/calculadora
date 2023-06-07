6dbeec709ce7958f9275da499032f0dba2e0f19c
Ricardo Luiz A. Pereira <manayba@gmail.com>
2023-06-07 16:26:03 -0300

    2.1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function calcular() {
     let num1 = parseFloat(document.getElementById("num1").value);
-    let    num2 = parseFloat(document.getElementById("num2").value);
-    let    operacao = document.getElementById("operacao").value;
-    let    resultado = document.getElementById("resultado");
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let operacao = document.getElementById("operacao").value;
+    let resultado = document.getElementById("resultado");
 
     switch (operacao) {
     case "+":
